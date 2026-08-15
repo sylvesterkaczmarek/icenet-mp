e8c2a646378849af3a2c76dda79b0d1c21b9b8ba
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-15 01:35:42 +0100

    Set CNN output channels before base init

--- icenet_mp/models/encoders/cnn_encoder.py
+++ icenet_mp/models/encoders/cnn_encoder.py
@@ -4,7 +4,7 @@ from typing import Any
 from torch import nn
 
 from icenet_mp.models.common import ConvBlockDownsample, ResizingInterpolation
-from icenet_mp.types import TensorNCHW
+from icenet_mp.types import DataSpace, TensorNCHW
 
 from .base_encoder import BaseEncoder
 
@@ -27,6 +27,7 @@ class CNNEncoder(BaseEncoder):
     def __init__(
         self,
         *,
+        data_space_in: DataSpace,
         activation: str = "ReLU",
         kernel_size: int = 3,
         n_layers: int = 3,
@@ -35,7 +36,12 @@ class CNNEncoder(BaseEncoder):
         **kwargs: Any,
     ) -> None:
         """Initialise a CNNEncoder."""
-        super().__init__(**kwargs)
+        output_channels = data_space_in.channels * scale_factor**n_layers
+        super().__init__(
+            data_space_in=data_space_in,
+            output_channels=output_channels,
+            **kwargs,
+        )
 
         # Construct list of layers
         layers: list[nn.Module] = []
@@ -73,9 +79,6 @@ class CNNEncoder(BaseEncoder):
                 n_channels,
             )
             n_channels *= scale_factor
-
-        # Set the number of output channels correctly
-        self.data_space_out.channels = n_channels
 
         # Combine the layers sequentially
         self.model = nn.Sequential(*layers)
